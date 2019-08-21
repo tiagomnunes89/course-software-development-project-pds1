d3e2ac0691fece406382e3830b9373a1a375f799
Tiago <tiagomnunes@hotmail.com>
2019-08-20 22:27:30 -0300

    project started

---
+++ src/main/java/com/tiagomnunes/aulapds1/Aulapds1Application.java
@@ -0,0 +1,13 @@
+package com.tiagomnunes.aulapds1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aulapds1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aulapds1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiagomnunes/aulapds1/Aulapds1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tiagomnunes.aulapds1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aulapds1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
