6fd906e954ef30126475dd35567e46544aadea31
Tiago <tiagomnunes@hotmail.com>
2019-08-27 20:12:38 -0300

    User entity and resource

--- src/main/java/com/tiagomnunes/aulapds1/Aulapds1Application.java
+++ src/main/java/com/tiagomnunes/aulapds1/Aulapds1Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Aulapds1Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Aulapds1Application.class, args);
+        String teste;
     }
 
 }
