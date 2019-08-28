6fd055e80d1711f7d2ae9ffdc7297419295c9ef8
Tiago <tiagomnunes@hotmail.com>
2019-08-27 21:53:23 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/tiagomnunes/aulapds1/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.tiagomnunes.aulapds1.repositories;
+
+import com.tiagomnunes.aulapds1.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+
+}
