bfd83d1da1b60c59c70ba89febf661dae3ccaf61
Tiago <tiagomnunes@hotmail.com>
2019-08-27 22:19:16 -0300

    Service layer, component registration

---
+++ src/main/java/com/tiagomnunes/aulapds1/services/UserService.java
@@ -0,0 +1,25 @@
+package com.tiagomnunes.aulapds1.services;
+
+import com.tiagomnunes.aulapds1.entities.User;
+import com.tiagomnunes.aulapds1.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+}
