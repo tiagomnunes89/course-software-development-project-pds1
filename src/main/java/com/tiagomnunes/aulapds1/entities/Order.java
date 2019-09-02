bf906b67abc458089cb9057ae035e3a6e5c53fb6
Tiago <tiagomnunes@hotmail.com>
2019-09-01 22:51:28 -0300

    OrderStatus enum

--- src/main/java/com/tiagomnunes/aulapds1/entities/Order.java
+++ src/main/java/com/tiagomnunes/aulapds1/entities/Order.java
@@ -1,6 +1,7 @@
 package com.tiagomnunes.aulapds1.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.tiagomnunes.aulapds1.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -22,12 +23,15 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -53,6 +57,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
