0b17f1c573fbefa27ca8d68ffa38bcdc93b96f35
Tiago <tiagomnunes@hotmail.com>
2019-09-10 21:44:30 -0300

    Exception handling - findById

--- src/main/java/com/tiagomnunes/aulapds1/services/UserService.java
+++ src/main/java/com/tiagomnunes/aulapds1/services/UserService.java
@@ -2,6 +2,7 @@ package com.tiagomnunes.aulapds1.services;
 
 import com.tiagomnunes.aulapds1.entities.User;
 import com.tiagomnunes.aulapds1.repositories.UserRepository;
+import com.tiagomnunes.aulapds1.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
