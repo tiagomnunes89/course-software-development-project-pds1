1485a2f38d2dec345e72be427cc4867b497fe38e
Tiago <tiagomnunes@hotmail.com>
2019-09-10 22:01:43 -0300

    Exception handling - delete

--- src/main/java/com/tiagomnunes/aulapds1/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/tiagomnunes/aulapds1/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.tiagomnunes.aulapds1.resources.exceptions;
 
+import com.tiagomnunes.aulapds1.services.exceptions.DatabaseException;
 import com.tiagomnunes.aulapds1.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,15 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(
+                Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError standardError = new StandardError(
                 Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(standardError);
--- src/main/java/com/tiagomnunes/aulapds1/services/UserService.java
+++ src/main/java/com/tiagomnunes/aulapds1/services/UserService.java
@@ -2,8 +2,11 @@ package com.tiagomnunes.aulapds1.services;
 
 import com.tiagomnunes.aulapds1.entities.User;
 import com.tiagomnunes.aulapds1.repositories.UserRepository;
+import com.tiagomnunes.aulapds1.services.exceptions.DatabaseException;
 import com.tiagomnunes.aulapds1.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
