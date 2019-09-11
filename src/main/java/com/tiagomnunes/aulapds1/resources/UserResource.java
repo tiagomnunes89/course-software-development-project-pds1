644cc61168a8f663bd747cccb49164a1b8a38981
Tiago <tiagomnunes@hotmail.com>
2019-09-10 21:01:19 -0300

    User delete

--- src/main/java/com/tiagomnunes/aulapds1/resources/UserResource.java
+++ src/main/java/com/tiagomnunes/aulapds1/resources/UserResource.java
@@ -11,30 +11,36 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
     @Autowired
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<User>> findAll() {
         List<User> userList = service.findAll();
         return ResponseEntity.ok().body(userList);
     }
 
-    @GetMapping(value="/{id}")
-    public ResponseEntity<User> findById(@PathVariable  Long id){
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
 
 
     @PostMapping
-    public ResponseEntity<User> insert (@RequestBody User user){
+    public ResponseEntity<User> insert(@RequestBody User user) {
         user = service.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
