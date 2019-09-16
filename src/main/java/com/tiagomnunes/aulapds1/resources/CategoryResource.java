f8d651d3682280a4d9127d53c69b7e80cdfd024f
Tiago <tiagomnunes@hotmail.com>
2019-09-16 19:26:35 -0300

    CRUD completo categoria

--- src/main/java/com/tiagomnunes/aulapds1/resources/CategoryResource.java
+++ src/main/java/com/tiagomnunes/aulapds1/resources/CategoryResource.java
@@ -1,34 +1,51 @@
 package com.tiagomnunes.aulapds1.resources;
 
 import com.tiagomnunes.aulapds1.entities.Category;
-import com.tiagomnunes.aulapds1.entities.User;
 import com.tiagomnunes.aulapds1.services.CategoryService;
-import com.tiagomnunes.aulapds1.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value="/categories")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @Autowired
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
+    public ResponseEntity<List<Category>> findAll() {
         List<Category> categoryList = service.findAll();
         return ResponseEntity.ok().body(categoryList);
     }
 
-    @GetMapping(value="/{id}")
-    public ResponseEntity<Category> findById(@PathVariable  Long id){
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
         Category category = service.findById(id);
         return ResponseEntity.ok().body(category);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category category) {
+        category = service.insert(category);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(category.getId()).toUri();
+        return ResponseEntity.created(uri).body(category);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category category) {
+        category = service.update(id, category);
+        return ResponseEntity.ok(category);
     }
 }
--- src/main/java/com/tiagomnunes/aulapds1/services/CategoryService.java
+++ src/main/java/com/tiagomnunes/aulapds1/services/CategoryService.java
@@ -2,9 +2,14 @@ package com.tiagomnunes.aulapds1.services;
 
 import com.tiagomnunes.aulapds1.entities.Category;
 import com.tiagomnunes.aulapds1.repositories.CategoryRepository;
+import com.tiagomnunes.aulapds1.services.exceptions.DatabaseException;
+import com.tiagomnunes.aulapds1.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,8 +23,36 @@ public class CategoryService {
         return repository.findAll();
     }
 
-    public Category findById(Long id){
-        Optional<Category> order = repository.findById(id);
-        return order.get();
+    public Category findById(Long id) {
+        Optional<Category> category = repository.findById(id);
+        return category.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Category insert(Category category) {
+        return repository.save(category);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Category update(Long id, Category category) {
+        try {
+            Category entity = repository.getOne(id);
+            updateData(entity, category);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Category entity, Category category) {
+        entity.setName(category.getName());
     }
 }
