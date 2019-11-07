686787cabb813a9f6da83bbcc8313499b6da973f
Tiago M. Nunes <37198181+tiagomnunes89@users.noreply.github.com>
2019-11-07 20:30:51 -0300

    Payment PUT POST endpoints

--- src/main/java/com/tiagomnunes/aulapds1/resources/PaymentResource.java
+++ src/main/java/com/tiagomnunes/aulapds1/resources/PaymentResource.java
@@ -1,15 +1,15 @@
 package com.tiagomnunes.aulapds1.resources;
 
 import com.tiagomnunes.aulapds1.dto.PaymentDTO;
+import com.tiagomnunes.aulapds1.dto.PaymentDTO;
 import com.tiagomnunes.aulapds1.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,5 +31,21 @@ public class PaymentResource {
     public ResponseEntity<PaymentDTO> findById(@PathVariable Long id) {
         PaymentDTO PaymentDTO = service.findById(id);
         return ResponseEntity.ok().body(PaymentDTO);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<PaymentDTO> insert(@RequestBody PaymentDTO paymentDTO) {
+        paymentDTO = service.insert(paymentDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(paymentDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(paymentDTO);
+    }
+    
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PaymentDTO> update(@PathVariable Long id, @RequestBody PaymentDTO paymentDTO) {
+        paymentDTO = service.update(id, paymentDTO);
+        return ResponseEntity.ok(paymentDTO);
     }
 }
--- src/main/java/com/tiagomnunes/aulapds1/services/PaymentService.java
+++ src/main/java/com/tiagomnunes/aulapds1/services/PaymentService.java
@@ -1,14 +1,16 @@
 package com.tiagomnunes.aulapds1.services;
 
-import com.tiagomnunes.aulapds1.dto.CategoryDTO;
 import com.tiagomnunes.aulapds1.dto.PaymentDTO;
-import com.tiagomnunes.aulapds1.entities.Category;
+import com.tiagomnunes.aulapds1.entities.Order;
 import com.tiagomnunes.aulapds1.entities.Payment;
+import com.tiagomnunes.aulapds1.repositories.OrderRepository;
 import com.tiagomnunes.aulapds1.repositories.PaymentRepository;
 import com.tiagomnunes.aulapds1.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -19,6 +21,9 @@ public class PaymentService {
     @Autowired
     PaymentRepository paymentRepository;
 
+    @Autowired
+    OrderRepository orderRepository;
+
     public List<PaymentDTO> findAll() {
         List<Payment> list = paymentRepository.findAll();
         return list.stream().map(e -> new PaymentDTO(e)).collect(Collectors.toList());
@@ -28,5 +33,30 @@ public class PaymentService {
         Optional<Payment> payment = paymentRepository.findById(id);
         Payment entity = payment.orElseThrow(() -> new ResourceNotFoundException(id));
         return new PaymentDTO(entity);
+    }
+
+    @Transactional
+    public PaymentDTO insert(PaymentDTO paymentDTO) {
+        Order order = orderRepository.getOne(paymentDTO.getOrderId());
+        Payment payment = new Payment(null, paymentDTO.getMoment(), order);
+        order.setPayment(payment);
+        orderRepository.save(order);
+        return new PaymentDTO(order.getPayment());
+    }
+
+    @Transactional
+    public PaymentDTO update(Long id, PaymentDTO paymentDTO) {
+        try {
+            Payment entity = paymentRepository.getOne(id);
+            updateData(entity, paymentDTO);
+            entity = paymentRepository.save(entity);
+            return new PaymentDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Payment entity, PaymentDTO paymentDTO) {
+        entity.setMoment(paymentDTO.getMoment());
     }
 }
