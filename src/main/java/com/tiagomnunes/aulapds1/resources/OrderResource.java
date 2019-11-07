094b515f814131ae27737ff7705ace74f6df8482
Tiago M. Nunes <37198181+tiagomnunes89@users.noreply.github.com>
2019-11-07 19:31:04 -0300

    Place order endpoint

--- src/main/java/com/tiagomnunes/aulapds1/resources/OrderResource.java
+++ src/main/java/com/tiagomnunes/aulapds1/resources/OrderResource.java
@@ -8,11 +8,11 @@ import com.tiagomnunes.aulapds1.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.swing.*;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -52,5 +52,13 @@ public class OrderResource {
     public ResponseEntity<List<OrderDTO>> findByClientId(@PathVariable Long clientId) {
         List<OrderDTO> orderList = service.findByClientId(clientId);
         return ResponseEntity.ok().body(orderList);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> placeOrder(@RequestBody List<OrderItemDTO> dtoList) {
+        OrderDTO orderDTO = service.placeOrder(dtoList);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(orderDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(orderDTO);
     }
 }
--- src/main/java/com/tiagomnunes/aulapds1/services/OrderService.java
+++ src/main/java/com/tiagomnunes/aulapds1/services/OrderService.java
@@ -4,14 +4,19 @@ import com.tiagomnunes.aulapds1.dto.OrderDTO;
 import com.tiagomnunes.aulapds1.dto.OrderItemDTO;
 import com.tiagomnunes.aulapds1.entities.Order;
 import com.tiagomnunes.aulapds1.entities.OrderItem;
+import com.tiagomnunes.aulapds1.entities.Product;
 import com.tiagomnunes.aulapds1.entities.User;
+import com.tiagomnunes.aulapds1.entities.enums.OrderStatus;
+import com.tiagomnunes.aulapds1.repositories.OrderItemRepository;
 import com.tiagomnunes.aulapds1.repositories.OrderRepository;
+import com.tiagomnunes.aulapds1.repositories.ProductRepository;
 import com.tiagomnunes.aulapds1.repositories.UserRepository;
 import com.tiagomnunes.aulapds1.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -21,21 +26,27 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     @Autowired
-    private OrderRepository repository;
+    private OrderRepository orderRepository;
 
     @Autowired
     private AuthService authService;
 
     @Autowired
+    OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
     private UserRepository userRepository;
 
     public List<OrderDTO> findAll() {
-        List<Order> list = repository.findAll();
+        List<Order> list = orderRepository.findAll();
         return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
     }
 
     public OrderDTO findById(Long id) {
-        Optional<Order> order = repository.findById(id);
+        Optional<Order> order = orderRepository.findById(id);
         Order entity = order.orElseThrow(() -> new ResourceNotFoundException(id));
         authService.validateOwnOrderOrAdmin(entity);
         return new OrderDTO(entity);
@@ -43,13 +54,13 @@ public class OrderService {
 
     public List<OrderDTO> findByClient () {
         User client = authService.authenticated();
-        List<Order> list = repository.findByClient(client);
+        List<Order> list = orderRepository.findByClient(client);
         return list.stream().map(e-> new OrderDTO(e)).collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
     public List<OrderItemDTO> findItems(Long id) {
-        Order order = repository.getOne(id);
+        Order order = orderRepository.getOne(id);
         authService.validateOwnOrderOrAdmin(order);
         Set<OrderItem> set = order.getItems();
         return set.stream().map(e -> new OrderItemDTO(e)).collect(Collectors.toList());
@@ -58,7 +69,22 @@ public class OrderService {
     @Transactional(readOnly = true)
     public List<OrderDTO> findByClientId(Long clientId) {
         User client = userRepository.getOne(clientId);
-        List<Order> list = repository.findByClient(client);
+        List<Order> list = orderRepository.findByClient(client);
         return list.stream().map(e-> new OrderDTO(e)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderDTO placeOrder(List<OrderItemDTO> dtoList) {
+        User client = authService.authenticated();
+        Order order = new Order(null, Instant.now(), OrderStatus.WAITING_PAYMENT, client);
+        for (OrderItemDTO itemDTO : dtoList){
+            Product product = productRepository.getOne(itemDTO.getProductId());
+            OrderItem item = new OrderItem(order, product, itemDTO.getQuantity(), itemDTO.getPrice());
+            order.getItems().add(item);
+        }
+        orderRepository.save(order);
+        orderItemRepository.saveAll(order.getItems());
+
+        return new OrderDTO(order);
     }
 }
